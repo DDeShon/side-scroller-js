7b3ebceede0ab873cbb6ea916124198a5e729f1f
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-01-26 09:40:38 -0600

    working on character class

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ const context = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+const gravity = 0.5;
 class Player {
   constructor() {
     this.position = {
       x: 50,
       y: 50,
+    };
+    this.velocity = {
+      x: 0,
+      y: 1,
     };
     this.width = 50;
     this.height = 50;
@@ -19,7 +24,20 @@ class Player {
     context.fillStyle = "blue";
     context.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
+
+  update() {
+    this.draw();
+    this.position.y += this.velocity.y;
+    this.velocity.y += gravity;
+  }
 }
 
 const player = new Player();
-player.draw();
+player.update();
+
+function animate() {
+  requestAnimationFrame(animate);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+}
+animate();
