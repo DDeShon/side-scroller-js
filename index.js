f92c54db6e409f4597f47a58509dfc412257d8a4
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-01-26 09:34:24 -0600

    setting up character class

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const canvas = document.querySelector("canvas");
 
 const context = canvas.getContext("2d");
 
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
 class Player {
   constructor() {
     this.position = {
       x: 50,
       y: 50,
     };
-    this.width = 100;
-    this.height = 100;
+    this.width = 50;
+    this.height = 50;
   }
 
   draw() {
+    context.fillStyle = "blue";
     context.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 }
